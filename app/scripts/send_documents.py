bd4d9a9c754976511b3f81369857dcbd36e995ac
Luca Strano <lucastrano2111@icloud.com>
2024-08-28 22:38:58 +0200

    Added send_documents.py

---
+++ app/scripts/send_documents.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import requests
+
+if len(sys.argv) < 2:
+    print("Usage: python send_documents.py <doc_path>")
+    sys.exit(1)
+
+path = sys.argv[1]
+docs = []
+
+# check if path is a directory
+if os.path.isdir(path):
+    print("Path is a directory")
+    docs.extend([os.path.join(path, f) \
+                 for f in os.listdir(path) \
+                 if f.endswith('.pdf')])
+elif path.endswith('.pdf'):
+    docs.append(path)
+
+for doc in docs:
+    # send the document with a POST request
+    filename = os.path.basename(doc)
+    doc_contents = open(doc, 'rb').read()
+    file = {"file": (filename, doc_contents)}
+    print(f"📄 Sending {filename} to PaperMage Service. This might take a while...")
+    try:
+        res = requests.post("http://localhost:8000/process", files=file)
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Request to PaperMage Service Failed.")
+        sys.exit(1)
+    if res.status_code == 200:
+        print(f"✅ {doc} Processed Successfully!")
+    else:
+        print(f"❌ {doc} Failed to Process.")
+        sys.exit(1)
