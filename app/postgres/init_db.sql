f6011590751ab42789b127838113705df8e24c3d
Luca Strano <lucastrano2111@icloud.com>
2024-09-17 16:55:26 +0200

    Switched to pgvector as postgres image

--- app/postgres/init_db.sql
+++ app/postgres/init_db.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
 CREATE TABLE papers (
     id VARCHAR(16) PRIMARY KEY,
     filename VARCHAR(100) NOT NULL,
@@ -7,15 +9,21 @@ CREATE TABLE papers (
 CREATE TABLE blocks (
     paper_id VARCHAR(16) NOT NULL,
     section_id VARCHAR(16) NOT NULL,
-    block VARCHAR(1000) NOT NULL,
+    content VARCHAR(1000) NOT NULL,
     PRIMARY KEY (paper_id, section_id),
     FOREIGN KEY (paper_id) REFERENCES papers(id)
 );
 
-CREATE TABLE citations ( 
-    paper_id VARCHAR(16) NOT NULL, 
-    citation_id VARCHAR(16) NOT NULL, 
-    citation_text VARCHAR(200) NOT NULL, 
-    PRIMARY KEY (paper_id, citation_id),
-    FOREIGN KEY (paper_id) REFERENCES papers(id)
+/*REATE TABLE chunks (
+    id BIGSERIAL PRIMARY KEY,
+    paper_id VARCHAR(16) NOT NULL,
+    content TEXT NOT NULL,
+    embedding vector(768),
 );
+
+CREATE TABLE images (
+    id BIGSERIAL NOT NULL,
+    content TEXT NOT NULL,
+    embedding vector(768),
+    PRIMARY KEY (paper_id, chunk_id)
+);*/
