9427a3b7ea8936ea885cc1c5f668e984088260ac
Luca Strano <lucastrano2111@icloud.com>
2024-10-22 16:18:16 +0200

    Added user_session variables

--- app/front/app.py
+++ app/front/app.py
@@ -1,2 +1,30 @@
 import chainlit as cl
+from papermage.recipes import CoreRecipe
+import psycopg2 as pg
+import os
 
+@cl.on_chat_start
+async def on_chat_start():
+
+    recipe = CoreRecipe()
+
+    db_name = os.environ.get("POSTGRES_DB")
+    db_user = os.environ.get("POSTGRES_USER")
+    db_password = os.environ.get("POSTGRES_PASSWORD")
+
+    # Ensure environment variables are set
+    if not db_name or not db_user or not db_password:
+        raise ValueError("Database environment variables are not set")
+    
+    connection = pg.connect(
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        host="localhost",
+        port="5432"
+    )
+    cursor = connection.cursor()
+
+    cl.user_session.set("recipe", recipe)
+    cl.user_session.set("connection", connection)
+    cl.user_session.set("cursor", cursor)
