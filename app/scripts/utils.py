d551789debd868ee83de3146d7eee318eb16e18b
Luca Strano <lucastrano2111@icloud.com>
2025-02-25 19:36:46 +0100

    Integrated db utils in main.py

--- app/scripts/utils.py
+++ app/scripts/utils.py
@@ -3,7 +3,7 @@ import hashlib
 
 ### --- DB UTILITY FUNCTIONS --- ###
 
-def calculate_hash(content : bytes, buffer_size : int = 4096) -> str:
+async def calculate_hash(content : bytes, buffer_size : int = 4096) -> str:
     """Returns the truncated hash of the content using a buffer with `buffer_size` chunks."""
     hash_obj = hashlib.md5()
     for i in range(0, len(content), buffer_size):
@@ -13,13 +13,13 @@ def calculate_hash(content : bytes, buffer_size : int = 4096) -> str:
     truncated_hash = full_hash[:len(full_hash)//2]
     return truncated_hash
 
-def does_file_exist(cursor: sqlite3.Cursor, file_id: str) -> bool:
+async def does_file_exist(cursor: sqlite3.Cursor, file_id: str) -> bool:
     """Check if a file with the given ID exists in the database"""
     cursor.execute("SELECT name FROM papers WHERE id = ?", (file_id,))
     result = cursor.fetchone()
     return result is not None
     
-def save_file_to_db(cursor : sqlite3.Cursor, connection : sqlite3.Connection, file_id: str, file_name: str) -> None:
+async def save_file_to_db(cursor : sqlite3.Cursor, connection : sqlite3.Connection, file_id: str, file_name: str) -> None:
     """Save the file to the database. Raises an exception if unsuccessful."""
     cursor.execute("INSERT INTO papers (id, name) VALUES (?, ?)", (file_id, file_name))
     cursor.connection.commit()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import chainlit as cl
 from app.scraper.papermage_scraper import PapermageScraper
 from app.scraper.scraper import Scraper, ImageData
+from app.scripts.utils import calculate_hash, does_file_exist, save_file_to_db
 from typing import Tuple
 import asyncio
 from functools import partial
+import sqlite3
 
 @cl.step(type="tool")
 async def scrape_pdf(scraper : Scraper, file_path : str) -> Tuple[str, ImageData]:
@@ -11,6 +13,8 @@ async def scrape_pdf(scraper : Scraper, file_path : str) -> Tuple[str, ImageData
 
 @cl.on_chat_start
 async def on_chat_start():
+    conn = sqlite3.connect("app/storage/sqlite/contents.db")
+    cl.user_session.set("db_conn", conn)
     cl.user_session.set("scraper", PapermageScraper())
     await cl.Message(content="Hi, I'm PaperSage! Send a file to start or check from the list of processed files.").send()
 
@@ -21,14 +25,26 @@ async def main(message: cl.Message):
             file_path = element.path
             if file_path is not None:
                 try:
-                    processing_msg = await cl.Message(content=f"Processing document \"{element.name}\"...")
-                    processing_msg.send()
+                    processing_msg = cl.Message(content=f"Processing document \"{element.name}\"...")
+                    await processing_msg.send()
 
-                    # step 1. convert pdf to markdown, extract figures and captions
+                    # step 1. add file to sqlite db if not processed
+                    conn = cl.user_session.get("db_conn")
+                    cursor = conn.cursor()
+                    with open(file_path, "rb") as f:
+                        file_content = f.read()
+                    file_id = await calculate_hash(file_content)
+                    if await does_file_exist(cursor, file_id):
+                        await cl.Message(content="This file has already been processed!").send()
+                        return
+                    await save_file_to_db(cursor, conn, file_id, element.name)
+                    
+
+                    # step 2. convert pdf to markdown, extract figures and captions
                     scraper = cl.user_session.get("scraper")
                     md, img_data = await scrape_pdf(scraper, file_path)
 
-                    # step 2. process md, img_data
+                    # step 3. process md, img_data
                     # TODO
                     
                     # Update processing message to show completion
