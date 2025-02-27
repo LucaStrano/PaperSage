bc364946c886d253f8baf3bc5cdcc699d53fe3d5
Luca Strano <lucastrano2111@icloud.com>
2025-02-27 20:03:15 +0100

    Add paper selection in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import chainlit as cl
+from chainlit.input_widget import Select
 from app.scraper.papermage_scraper import PapermageScraper
 from app.scraper.scraper import Scraper, ImageData
 from app.scripts.utils import calculate_hash, does_file_exist, save_file_to_db
@@ -6,27 +7,62 @@ from typing import Tuple
 import asyncio
 from functools import partial
 import sqlite3
+from typing import List
+
+## -- UTILITY FUNCTIONS -- ##
+
+async def get_avaliable_papers(connection: sqlite3.Connection) -> List[Tuple[str, str]]:
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, name FROM papers")
+    papers = cursor.fetchall()
+    return papers
+
+async def generate_settings(papers: List[Tuple[str, str]]) -> cl.ChatSettings:
+    proc_papers_list = [f"{paper[0]} - {paper[1]}" for paper in papers]
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="paper",
+                label="Select a paper",
+                values=proc_papers_list,
+                initial_index=0
+            )
+        ]
+    ).send()
+    return settings
+
+## -- CHAINLIT TOOLS -- ##
 
 @cl.step(type="tool")
-async def scrape_pdf(scraper : Scraper, file_path : str) -> Tuple[str, ImageData]:
+async def process_pdf(scraper : Scraper, file_path : str) -> Tuple[str, ImageData]:
     return scraper.process_document(document_path=file_path)
+
+## -- CHAINLIT MAIN FUNCITONS -- ##
 
 @cl.on_chat_start
 async def on_chat_start():
+
     conn = sqlite3.connect("app/storage/sqlite/contents.db")
     cl.user_session.set("db_conn", conn)
     cl.user_session.set("scraper", PapermageScraper())
-    await cl.Message(content="Hi, I'm PaperSage! Send a file to start or check from the list of processed files.").send()
+
+    await cl.Message(content="Hi, I'm PaperSage! Send a file to start or select one from the list of processed files.").send()
+
+    papers = await get_avaliable_papers(conn)
+    if len(papers) > 0:
+        papers = await get_avaliable_papers(conn)
+        settings = await generate_settings(papers)
+        cl.user_session.set("paper_settings", settings)
+        proc_papers_str = "\n".join(f"{i+1}. {paper[0]} - {paper[1]}" for i, paper in enumerate(papers))
+        await cl.Message(content=f"Here are the files you have processed:\n {proc_papers_str}").send()
 
 @cl.on_message
 async def main(message: cl.Message):
-    if len(message.elements) > 0:
+    if message.elements:
         for element in message.elements:
-            file_path = element.path
-            if file_path is not None:
+            if element.mime == 'pdf':
+                file_path = element.path
                 try:
-                    processing_msg = cl.Message(content=f"Processing document \"{element.name}\"...")
-                    await processing_msg.send()
 
                     # step 1. add file to sqlite db if not processed
                     conn = cl.user_session.get("db_conn")
@@ -42,14 +78,14 @@ async def main(message: cl.Message):
 
                     # step 2. convert pdf to markdown, extract figures and captions
                     scraper = cl.user_session.get("scraper")
-                    md, img_data = await scrape_pdf(scraper, file_path)
+                    md, img_data = await process_pdf(scraper, file_path)
 
                     # step 3. process md, img_data
                     # TODO
-                    
-                    # Update processing message to show completion
-                    processing_msg.content = f"Document \"{element.name}\" processed!"
-                    await processing_msg.update()
+
+                    await cl.Message(content=f"Document \"{element.name}\" processed!").send()
                     
                 except Exception as e:
                     await cl.Message(content=f"Error processing document: {str(e)}").send()
+    else:
+        await cl.Message(content=f"Textual Message Received!").send()
