04082416fa4bf13deb518d3c021d1e7d068f507e
Luca Strano <lucastrano2111@icloud.com>
2025-03-01 00:24:59 +0100

    Add qdrant init, embedding model selection

--- app/scripts/init_db.py
+++ app/scripts/init_db.py
@@ -1,15 +1,16 @@
 import os
 import sqlite3
+from qdrant_client import QdrantClient, models
+import yaml
 
-# Create the directory if it doesn't exist
+# SQLITE3 CONFIG
+print("Creating SQLite3 database...")
 dbdir_path = os.path.join("app", "storage", "sqlite")
 os.makedirs(dbdir_path, exist_ok=True)
 
-# Create the SQLite database file
 conn = sqlite3.connect(os.path.join(dbdir_path, "contents.db"))
 c = conn.cursor()
 
-# papers table
 c.execute("""\
 CREATE TABLE IF NOT EXISTS papers (
     id VARCHAR(32) PRIMARY KEY, 
@@ -20,3 +21,22 @@ CREATE TABLE IF NOT EXISTS papers (
 conn.commit()
 c.close()
 conn.close()
+print("✅ SQLite3 database created.")
+
+# QDRANT CONFIG
+print("Creating Qdrant collection...")
+settings = yaml.safe_load(open("config.yaml", "r"))
+print(settings)
+qdrantdir_path = os.path.join("app", "storage", "qdrant")
+os.makedirs(qdrantdir_path, exist_ok=True)
+
+client = QdrantClient(path=os.path.join(qdrantdir_path, "vectorstore"))
+client.create_collection(
+    collection_name=settings['configs'][1]['qdrant_config'][0]['collection_name'], 
+    vectors_config=models.VectorParams(
+        size=settings['configs'][2]['embedding_config'][2]['output_length'], 
+        distance=models.Distance.COSINE
+    ),
+)
+client.close()
+print("✅ Qdrant collection created.")
