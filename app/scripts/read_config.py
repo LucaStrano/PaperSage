ab2429f9b4b8509fbd66fef0acb187ae2db57da5
Luca Strano <lucastrano2111@icloud.com>
2025-03-01 00:51:09 +0100

    Add langchain requirement

--- app/scripts/read_config.py
+++ app/scripts/read_config.py
@@ -6,9 +6,11 @@ def get_config():
         chainlit_config = config['configs'][0]['chainlit_config']
         host = chainlit_config[0]['host']
         port = chainlit_config[1]['port']
-        return host, port
+        ollama_emb_model = config['configs'][2]['embedding_config'][0]['model']
+        return host, port, ollama_emb_model
 
 if __name__ == "__main__":
-    host, port = get_config()
+    host, port, olama_emb_model = get_config()
     print(f"export CHAINLIT_HOST={host}")
     print(f"export CHAINLIT_PORT={port}")
+    print(f"export OLLAMA_EMB_MODEL={olama_emb_model}")
