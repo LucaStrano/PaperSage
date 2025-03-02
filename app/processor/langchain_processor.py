4d5bba3a7a9ddac03f33bc1e963d25195edbe447
Luca Strano <lucastrano2111@icloud.com>
2025-03-02 18:05:00 +0100

    Add chunking penalty config

--- app/processor/langchain_processor.py
+++ app/processor/langchain_processor.py
@@ -7,6 +7,7 @@ from typing import Dict, Any
 import yaml
 from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 from transformers import AutoTokenizer, PreTrainedTokenizer, PreTrainedTokenizerFast
+from langchain_core.documents import Document
 
 class LangchainProcessor(Processor):
     """
@@ -24,7 +25,11 @@ class LangchainProcessor(Processor):
         self.chunk_overlap = int(self.chunk_size * self.configs['chunk_overlap_percent'])
         self.markdown_splitter = MarkdownHeaderTextSplitter([("##", "chapter")])
         self.token_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-            self.tokenizer, chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
+            separators=['. '], # split by sentences
+            keep_separator=True,
+            tokenizer=self.tokenizer, 
+            chunk_size= self.chunk_size - self.configs['chunk_size_penalty'],
+            chunk_overlap=self.chunk_overlap
         )
 
     def load_tokenizer_model(self, model_name : str) -> PreTrainedTokenizer|PreTrainedTokenizerFast:
@@ -39,7 +44,6 @@ class LangchainProcessor(Processor):
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
         
-        # Extract specific configurations
         embedding_config = config['configs'][2]['embedding_config']
         chunking_config = config['configs'][3]['chunking_config']
         
@@ -49,8 +53,22 @@ class LangchainProcessor(Processor):
             'max_seq_length': embedding_config[2]['max_seq_length'],
             'use_emb_model_max_seq_length': chunking_config[0]['use_emb_model_max_seq_length'],
             'chunk_size': chunking_config[1]['chunk_size'],
-            'chunk_overlap_percent': chunking_config[2]['chunk_overlap_percent']
+            'chunk_size_penalty': chunking_config[2]['chunk_size_penalty'],
+            'chunk_overlap_percent': chunking_config[3]['chunk_overlap_percent']
         }
+    
+    def split_md_data(self, md_data : str) -> List[Document]:
+        """
+        Splits markdown data into chunks.
+        Args:
+            md_data (str): Markdown formatted text.
+        Returns:
+            List[Document]: List of Document objects. First object is paper info.
+        """
+        texts = self.markdown_splitter.split_text(md_data)
+        splits = self.token_splitter.split_documents(texts[1:])
+        splits.insert(0, texts[0]) # Add paper info to the beginning of the list
+        return splits
 
     def process(self, md_data: str, image_data : ImageData, client : QdrantClient) -> None:
         """
@@ -60,4 +78,4 @@ class LangchainProcessor(Processor):
             image_data (ImageData): Image data.
             client: Qdrant Vector Store client.
         """
-        pass
+        md_splits = self.split_md_data(md_data)
