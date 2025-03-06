d9f5c2d8f97491fa2c06b5367e961d9a31294ff0
Luca Strano <lucastrano2111@icloud.com>
2025-03-06 21:02:50 +0100

    Implement full document handling pipeline

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import chainlit as cl
 from chainlit.input_widget import Select
+from chainlit.element import Element
+
 from app.scraper.papermage_scraper import PapermageScraper
+from app.processor.langchain_processor import LangchainProcessor
 from app.scraper.scraper import Scraper, ImageData
 from app.scripts.utils import calculate_hash, does_file_exist, save_file_to_db
-from typing import Tuple
-import asyncio
-from functools import partial
+from app.config_loader import ConfigLoader
+
+from typing import List, Tuple
+
 import sqlite3
-from typing import List
+from qdrant_client import QdrantClient
+from langchain_ollama import OllamaEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from langchain_core.vectorstores import VectorStore
+
+from transformers import AutoTokenizer, AutoModel, AutoImageProcessor
 
 ## -- UTILITY FUNCTIONS -- ##
 
@@ -37,6 +46,14 @@ async def generate_settings(papers: List[Tuple[str, str]]) -> cl.ChatSettings:
 async def process_pdf(scraper : Scraper, file_path : str) -> Tuple[str, ImageData]:
     return scraper.process_document(document_path=file_path)
 
+@cl.step(type="tool")
+async def send_file(processor : LangchainProcessor) -> bool:
+    try:
+        processor.process()
+        return True
+    except Exception as e:
+        await cl.Message(content=f"Error processing document: {str(e)}. Try again").send()
+
 ## -- CHAINLIT MAIN FUNCITONS -- ##
 
 @cl.on_chat_start
@@ -44,17 +61,40 @@ async def on_chat_start():
 
     conn = sqlite3.connect("app/storage/sqlite/contents.db")
     cl.user_session.set("db_conn", conn)
+    configs = ConfigLoader().get_config()
+    cl.user_session.set("configs", configs)
     cl.user_session.set("scraper", PapermageScraper())
 
-    await cl.Message(content="Hi, I'm PaperSage! Send a file to start or select one from the list of processed files.").send()
+    text_embed = OllamaEmbeddings(model=configs['embedding_config']['model'])
+    cl.user_session.set("text_embed", text_embed)
+    qdrant_client = QdrantClient(path='app/storage/qdrant/vectorstore')
+    text_vs : VectorStore = QdrantVectorStore(
+        client=qdrant_client,
+        collection_name='paper_texts',
+        embedding=text_embed
+    )
+    cl.user_session.set("text_vs", text_vs)
+    img_proc = AutoImageProcessor.from_pretrained("nomic-ai/nomic-embed-vision-v1.5")
+    img_emb = AutoModel.from_pretrained("nomic-ai/nomic-embed-vision-v1.5", trust_remote_code=True)
+    cl.user_session.set("img_proc", img_proc)
+    cl.user_session.set("img_emb", img_emb)
 
     papers = await get_avaliable_papers(conn)
-    if len(papers) > 0:
+    if len(papers) == 0:
+        while True:
+            files = await cl.AskFileMessage(
+                content="Please upload a PDF file to begin!", 
+                accept=["application/pdf"],
+                max_size_mb=50
+            ).send()
+            if files: break
+        # await cl.Message(content=f"Sent file.").send()
+        await main(cl.Message(content='',elements=[Element(mime='pdf', name=files[0].name, path=files[0].path)]))
+    else:
         papers = await get_avaliable_papers(conn)
         settings = await generate_settings(papers)
         cl.user_session.set("paper_settings", settings)
-        proc_papers_str = "\n".join(f"{i+1}. {paper[0]} - {paper[1]}" for i, paper in enumerate(papers))
-        await cl.Message(content=f"Here are the files you have processed:\n {proc_papers_str}").send()
+        await cl.Message(content=f"Hi! Select a paper from the list to start asking questions.").send()
 
 @cl.on_message
 async def main(message: cl.Message):
@@ -83,7 +123,16 @@ async def main(message: cl.Message):
                     md, img_data = await process_pdf(scraper, file_path)
 
                     # step 3. process md, img_data
-                    # TODO
+                    processor = LangchainProcessor(
+                        md,
+                        img_data,
+                        file_id,
+                        conn,
+                        cl.user_session.get("text_vs"),
+                        cl.user_session.get("img_emb"),
+                        cl.user_session.get("img_proc")
+                    )
+                    res = await send_file(processor)
 
                     # step 4. add new paper to settings
                     papers = await get_avaliable_papers(conn)
